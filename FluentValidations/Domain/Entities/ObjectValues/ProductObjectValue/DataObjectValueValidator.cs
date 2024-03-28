9dfd877861e06cc02436b58429064c5dd735d25c
Rafael Silva <ainbot6@gmail.com>
2024-03-28 17:39:04 -0300

    feat: create DataObjectValueValidator.cs

---
+++ FluentValidations/Domain/Entities/ObjectValues/ProductObjectValue/DataObjectValueValidator.cs
@@ -0,0 +1,18 @@
+﻿using Domain.Entities.ObjectValues.ProductObjectValue;
+using FluentValidation;
+
+namespace FluentValidations.Domain.Entities.ObjectValues.ProductObjectValue;
+
+public class DataObjectValueValidator : AbstractValidator<DataObjectValue>
+{
+    public DataObjectValueValidator()
+    {
+        RuleFor(x => x.ReleaseMonth)
+            .NotEmpty().WithMessage("Release month can't be empty.")
+            .Length(4, 9).WithMessage("Release Month maximum 9 characters.");
+
+        RuleFor(x => x.ReleaseYear)
+            .NotEmpty().WithMessage("Release Year is required.")
+            .InclusiveBetween(4,4).WithMessage("Must be a 4-digit positive integer.");
+    }
+}
