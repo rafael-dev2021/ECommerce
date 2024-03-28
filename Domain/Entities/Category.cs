1f0d743d52d0692cdd67e29dea262fc257d621ae
Rafael Silva <ainbot6@gmail.com>
2024-03-28 15:53:47 -0300

    feat: create category.cs

---
+++ Domain/Entities/Category.cs
@@ -0,0 +1,10 @@
+﻿namespace Domain.Entities;
+
+public sealed class Category(int id, string name, List<string> imagesUrl, bool isActive)
+{
+    public int Id { get; private set; } = id;
+    public string Name { get; private set; } = name;
+    public List<string> ImagesUrl { get; private set; } = imagesUrl;
+    public bool IsActive { get; private set; } = isActive;
+    public ICollection<Product> Products { get; } = [];
+}
