f6b5b8cb573ed2a447bc27ed8765e887d26daae9
Rafael Silva <ainbot6@gmail.com>
2024-03-28 17:41:49 -0300

    feat: create FlagsObjectValueValidator.cs

---
+++ FluentValidations/Domain/Entities/ObjectValues/ProductObjectValue/FlagsObjectValueValidator.cs
@@ -0,0 +1,18 @@
+﻿using Domain.Entities.ObjectValues.ProductObjectValue;
+using FluentValidation;
+
+namespace FluentValidations.Domain.Entities.ObjectValues.ProductObjectValue;
+
+public class FlagsObjectValueValidator: AbstractValidator<FlagsObjectValue>
+{
+    public FlagsObjectValueValidator()
+    {
+        RuleFor(x => x.IsFavorite)
+            .NotEqual(true).When(x => x.IsDailyOffer)
+            .WithMessage("Cannot be favorite and daily offer at the same time.");
+
+        RuleFor(x => x.IsBestSeller)
+            .NotEqual(true).When(x => x.IsDailyOffer)
+            .WithMessage("Cannot be best seller and daily offer at the same time.");
+    }
+}
