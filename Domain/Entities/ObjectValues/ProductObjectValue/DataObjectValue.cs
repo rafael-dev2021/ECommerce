a3bdb23460e3744bc6e79969311754f0210725db
Rafael Silva <ainbot6@gmail.com>
2024-03-28 17:37:26 -0300

    feat: create DataObjectValue.cs

---
+++ Domain/Entities/ObjectValues/ProductObjectValue/DataObjectValue.cs
@@ -0,0 +1,10 @@
+﻿namespace Domain.Entities.ObjectValues.ProductObjectValue;
+
+public class DataObjectValue
+{
+    public string ReleaseMonth { get; private set; } = string.Empty;
+    public int ReleaseYear { get; private set; }
+    
+    public void SetReleaseMonth(string month) => ReleaseMonth = month;
+    public void SetReleaseYear(int year) => ReleaseYear = year;
+}
