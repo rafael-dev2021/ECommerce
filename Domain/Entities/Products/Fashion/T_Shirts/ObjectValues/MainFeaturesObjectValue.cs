652b74232c60b4d2985e907d0a1c4e4e8207d92a
Rafael Silva <ainbot6@gmail.com>
2024-03-28 20:34:14 -0300

    feat: created value objects for the Shirt.cs class

--- Domain/Entities/Products/Fashion/T_Shirts/ObjectValues/MainFeaturesObjectValue.cs
+++ Domain/Entities/Products/Fashion/T_Shirts/ObjectValues/MainFeaturesObjectValue.cs
@@ -1,16 +1,10 @@
 ﻿namespace Domain.Entities.Products.Fashion.T_Shirts.ObjectValues;
 
-public class MainFeaturesObjectValue
+public class MainFeaturesObjectValue : CommonProperties
 {
-    public string Gender { get; private set; } = string.Empty;
-    public string Age { get; private set; } = string.Empty;
     public string TypeOfClothing { get; private set; } = string.Empty;
     public string FabricDesign { get; private set; } = string.Empty;
-    public string Size { get; private set; } = string.Empty;
     
-    public void SetGender(string gender) => Gender = gender;
-    public void SetAge(string age) => Age = age;
     public void SetTypeOfClothing(string typeOfClothing) => TypeOfClothing = typeOfClothing;
     public void SetFabricDesign(string fabricDesign) => FabricDesign = fabricDesign;
-    public void SetSize(string size) => Size = size;
 }
--- Domain/Entities/Products/Fashion/T_Shirts/ObjectValues/OtherFeaturesObjectValue.cs
+++ Domain/Entities/Products/Fashion/T_Shirts/ObjectValues/OtherFeaturesObjectValue.cs
@@ -2,14 +2,12 @@
 
 public class OtherFeaturesObjectValue
 {
-    public string RecommendedUses { get; private set; } = string.Empty;
     public string Composition { get; private  set; } = string.Empty;
     public string MainMaterial { get; private  set; } = string.Empty;
     public int UnitsPerKit { get; private  set; }
     public bool WithRecycledMaterials { get; private  set; }
     public bool ItsSporty { get; private  set; }
     
-    public void SetRecommendedUses(string recommendedUses) => RecommendedUses = recommendedUses;
     public void SetComposition(string composition) => Composition = composition;
     public void SetMainMaterial(string mainMaterial) => MainMaterial = mainMaterial;
     public void SetUnitsPerKit(int unitsPerKit) => UnitsPerKit = unitsPerKit;
