e9c84cd5ea0ba84715ac6dea8b961dbdbba8cb8e
Rafael Silva <ainbot6@gmail.com>
2024-03-28 15:54:27 -0300

    feat: create CategoryWithProductCount.cs

---
+++ Domain/Entities/CategoryWithProductCount.cs
@@ -0,0 +1,7 @@
+﻿namespace Domain.Entities;
+
+public class CategoryWithProductCount
+{
+    public string CategoryName { get; protected set; } = string.Empty;
+    public int ProductCount { get; protected set; } = 0;
+}
