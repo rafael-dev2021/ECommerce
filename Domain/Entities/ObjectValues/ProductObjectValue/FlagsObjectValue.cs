ceb1b580d3055b6a6dbbfe4b71c97943ff5864ab
Rafael Silva <ainbot6@gmail.com>
2024-03-28 17:37:45 -0300

    feat: create FlagsObjectValue.cs

---
+++ Domain/Entities/ObjectValues/ProductObjectValue/FlagsObjectValue.cs
@@ -0,0 +1,12 @@
+﻿namespace Domain.Entities.ObjectValues.ProductObjectValue;
+
+public class FlagsObjectValue
+{
+    public bool IsFavorite { get; private set; }
+    public bool IsDailyOffer { get; private set; }
+    public bool IsBestSeller { get; private set; }
+
+    public void SetIsFavorite(bool value) => IsFavorite = value;
+    public void SetIsDailyOffer(bool value) => IsDailyOffer = value;
+    public void SetIsBestSeller(bool value) => IsBestSeller = value;
+}
