0ce9df35178a11784741bb09766c63e987a2dc24
Rafael Silva <ainbot6@gmail.com>
2024-03-28 20:09:11 -0300

    feat: created value objects for the smartphone.cs class

---
+++ Domain/Entities/Products/Technology/Smartphones/ObjectValues/CameraObjectValue.cs
@@ -0,0 +1,14 @@
+﻿namespace Domain.Entities.Products.Technology.Smartphones.ObjectValues;
+
+public class CameraObjectValue
+{
+    public string MainCameraSpec { get; private set; } = string.Empty;
+    public string MainCameraFeature { get; private set; } = string.Empty;
+    public string SelfieCameraSpec { get; private set; } = string.Empty;
+    public string SelfieCameraFeature { get; private set; } = string.Empty;
+    
+    public void SetMainCameraSpec(string mainCameraSpec) => MainCameraSpec = mainCameraSpec;
+    public void SetMainCameraFeature(string mainCameraFeature) => MainCameraFeature = mainCameraFeature;
+    public void SetSelfieCameraSpec(string selfieCameraSpec) => SelfieCameraSpec = selfieCameraSpec;
+    public void SetSelfieCameraFeature(string selfieCameraFeature) => SelfieCameraFeature = selfieCameraFeature;
+}
---
+++ Domain/Entities/Products/Technology/Smartphones/ObjectValues/PlatformObjectValue.cs
@@ -0,0 +1,14 @@
+﻿namespace Domain.Entities.Products.Technology.Smartphones.ObjectValues;
+
+public class PlatformObjectValue
+{
+    public string OperatingSystem { get; private set; } = string.Empty;
+    public string Chipset { get; private  set; } = string.Empty;
+    public string Gpu { get; private  set; } = string.Empty;
+    public string Cpu { get; private  set; } = string.Empty;
+    
+    public void SetOperatingSystem(string operatingSystem) => OperatingSystem = operatingSystem;
+    public void SetChipset(string chipset) => Chipset = chipset;
+    public void SetGpu(string gpu) => Gpu = gpu;
+    public void SetCpu(string cpu) => Cpu = cpu;
+}
