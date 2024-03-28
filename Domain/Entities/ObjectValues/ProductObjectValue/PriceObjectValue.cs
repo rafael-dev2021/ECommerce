238651b3d29300b142ba27e5610a4593a93e4710
Rafael Silva <ainbot6@gmail.com>
2024-03-28 18:04:15 -0300

    feat: create PriceObjectValue.cs

---
+++ Domain/Entities/ObjectValues/ProductObjectValue/PriceObjectValue.cs
@@ -0,0 +1,10 @@
+﻿namespace Domain.Entities.ObjectValues.ProductObjectValue;
+
+public class PriceObjectValue
+{
+    public decimal Price { get; private set; }
+    public decimal HistoryPrice { get; private set; }
+
+    public void SetPrice(decimal price) => Price = price;
+    public void SetHistoryPrice(decimal historyPrice) => HistoryPrice = historyPrice;
+}
