05e1539e5e41228d7a1f66213cebffed52262e88
Rafael Silva <ainbot6@gmail.com>
2024-03-28 19:52:56 -0300

    feat: create Game.cs

---
+++ Domain/Entities/Products/Technology/Games/Game.cs
@@ -0,0 +1,77 @@
+﻿using Domain.Entities.ObjectValues.ProductObjectValue;
+using Domain.Entities.Products.Technology.Games.ObjectValues;
+
+namespace Domain.Entities.Products.Technology.Games;
+
+public sealed class Game : Product
+{
+    public Game(int id, string name, string description, List<string> imagesUrl, int stock, int categoryId) :
+        base(id, name, description, imagesUrl, stock, categoryId)
+    { }
+
+    public Game(
+        string name,
+        string description,
+        List<string> imagesUrl,
+        int stock,
+        DataObjectValue? dataObjectValue,
+        FlagsObjectValue? flagsObjectValue,
+        PriceObjectValue? priceObjectValue,
+        SpecificationObjectValue? specificationObjectValue,
+        WarrantyObjectValue? warrantyObjectValue,
+        GeneralFeaturesObjectValue generalFeaturesObjectValue,
+        MediaSpecificationObjectValue mediaSpecificationObjectValue,
+        RequirementObjectValue requirementObjectValue,
+        int categoryId) :
+        base(
+            name,
+            description,
+            imagesUrl,
+            stock, dataObjectValue,
+            flagsObjectValue,
+            priceObjectValue,
+            specificationObjectValue,
+            warrantyObjectValue,
+            categoryId)
+    {
+        GeneralFeaturesObjectValue = generalFeaturesObjectValue;
+        MediaSpecificationObjectValue = mediaSpecificationObjectValue;
+        RequirementObjectValue = requirementObjectValue;
+    }
+
+    public void UpdateGame(
+        string name,
+        string description,
+        List<string> imagesUrl,
+        int stock,
+        DataObjectValue? dataObjectValue,
+        FlagsObjectValue? flagsObjectValue,
+        PriceObjectValue? priceObjectValue,
+        SpecificationObjectValue? specificationObjectValue,
+        WarrantyObjectValue? warrantyObjectValue,
+        GeneralFeaturesObjectValue generalFeaturesObjectValue,
+        MediaSpecificationObjectValue mediaSpecificationObjectValue,
+        RequirementObjectValue requirementObjectValue,
+        int categoryId)
+    {
+        UpdateProduct(
+                name,
+                description,
+                imagesUrl,
+                stock,
+                dataObjectValue,
+                flagsObjectValue,
+                priceObjectValue,
+                specificationObjectValue,
+                warrantyObjectValue,
+                categoryId);
+        
+        GeneralFeaturesObjectValue = generalFeaturesObjectValue;
+        MediaSpecificationObjectValue = mediaSpecificationObjectValue;
+        RequirementObjectValue = requirementObjectValue;
+    }
+
+    public GeneralFeaturesObjectValue GeneralFeaturesObjectValue { get; private set; }
+    public MediaSpecificationObjectValue MediaSpecificationObjectValue { get; private set; }
+    public RequirementObjectValue RequirementObjectValue { get; private set; }
+}
