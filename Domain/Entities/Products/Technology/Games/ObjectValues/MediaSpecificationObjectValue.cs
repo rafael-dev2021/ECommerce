1c99bbfa76fe1eb70ec57926809da815ae2406e8
Rafael Silva <ainbot6@gmail.com>
2024-03-28 19:55:15 -0300

    feat: created value objects for the game.cs class

---
+++ Domain/Entities/Products/Technology/Games/ObjectValues/MediaSpecificationObjectValue.cs
@@ -0,0 +1,31 @@
+﻿namespace Domain.Entities.Products.Technology.Games.ObjectValues;
+
+public class MediaSpecificationObjectValue
+{
+    public string Format { get; private set; } = string.Empty;
+    public string AudioLanguages { get; private set; } = string.Empty;
+    public string SubtitleLanguages { get; private set; } = string.Empty;
+    public string ScreenLanguages { get; private set; } = string.Empty;
+    public int MaximumNumberOfOfflinePlayers { get; private set; }
+    public int MaximumNumberOfOnlinePlayers { get; private set; }
+    public int FileSize { get; private set; }
+    public bool IsMultiplayer { get; private set; }
+    public bool IsOnline { get; private set; }
+    public bool IsOffline { get; private set; }
+
+    public void SetFormat(string format) => Format = format;
+    public void SetAudioLanguages(string audioLanguages) => AudioLanguages = audioLanguages;
+    public void SetSubtitleLanguages(string subtitleLanguages) => SubtitleLanguages = subtitleLanguages;
+    public void SetScreenLanguages(string screenLanguages) => ScreenLanguages = screenLanguages;
+
+    public void SetMaximumNumberOfOfflinePlayers(int maximumNumberOfOfflinePlayers) =>
+        MaximumNumberOfOfflinePlayers = maximumNumberOfOfflinePlayers;
+
+    public void SetMaximumNumberOfOnlinePlayers(int maximumNumberOfOnlinePlayers) =>
+        MaximumNumberOfOnlinePlayers = maximumNumberOfOnlinePlayers;
+
+    public void SetFileSize(int fileSize) => FileSize = fileSize;
+    public void SetIsMultiplayer(bool isMultiplayer) => IsMultiplayer = isMultiplayer;
+    public void SetIsOnline(bool isOnline) => IsOnline = isOnline;
+    public void SetIsOffline(bool isOffline) => IsOffline = isOffline;
+}
---
+++ Domain/Entities/Products/Technology/Games/ObjectValues/RequirementObjectValue.cs
@@ -0,0 +1,15 @@
+﻿namespace Domain.Entities.Products.Technology.Games.ObjectValues;
+
+public class RequirementObjectValue
+{
+    public int MinimumRamRequirementInMb { get; private set; }
+    public string MinimumOperatingSystemsRequired { get; private set; } = string.Empty;
+    public string MinimumGraphicsProcessorsRequired { get; private set; } = string.Empty;
+    public string MinimumProcessorsRequired { get; private set; } = string.Empty;
+    
+    public void SetMinimumRamRequirementInMb(int value) => MinimumRamRequirementInMb = value;
+    public void SetMinimumOperatingSystemsRequired(string value) => MinimumOperatingSystemsRequired = value;
+    public void SetMinimumGraphicsProcessorsRequired(string value) => MinimumGraphicsProcessorsRequired = value;
+    public void SetMinimumProcessorsRequired(string value) => MinimumProcessorsRequired = value;
+
+}
