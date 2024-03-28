ebaa0cdd24d9cdb299a3ad12f299ff4da28bc275
Rafael Silva <ainbot6@gmail.com>
2024-03-28 18:05:32 -0300

    feat: create PriceObjectValueValidator.cs

---
+++ FluentValidations/Domain/Entities/ObjectValues/ProductObjectValue/PriceObjectValueValidator.cs
@@ -0,0 +1,16 @@
+﻿using Domain.Entities.ObjectValues.ProductObjectValue;
+using FluentValidation;
+
+namespace FluentValidations.Domain.Entities.ObjectValues.ProductObjectValue;
+
+public class PriceObjectValueValidator: AbstractValidator<PriceObjectValue>
+{
+    public PriceObjectValueValidator()
+    {
+        RuleFor(x => x.Price)
+            .GreaterThan(0).WithMessage("Price must be greater than zero.");
+
+        RuleFor(x => x.HistoryPrice)
+            .GreaterThanOrEqualTo(0).WithMessage("History price must be greater than or equal to zero.");
+    }
+}
