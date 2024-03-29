d245785fb98c0e08c77cd6cdbb1f65d45c9f09e8
Rafael Silva <ainbot6@gmail.com>
2024-03-29 00:13:25 -0300

    feat: unit test created for the FlagsObjectValueValidator.cs class

--- UnitTests/Domain/Entities/ObjectValues/ProductObjectValue/FlagsObjectValueTests.cs
+++ UnitTests/Domain/Entities/ObjectValues/ProductObjectValue/FlagsObjectValueTests.cs
@@ -5,20 +5,13 @@ using Xunit;
 
 namespace UnitTests.Domain.Entities.ObjectValues.ProductObjectValue;
 
-/// <summary>
-/// Unit tests for the <see cref="FlagsObjectValue"/> class and its validations.
-/// </summary>
+ [TestFixture]
 public class FlagsObjectValueTests
 {
-    /// <summary>
-    /// A validator for the <see cref="FlagsObjectValue"/> class.
-    /// </summary>
     private readonly FlagsObjectValueValidator _validator = new();
 
-    /// <summary>
-    /// Tests that the validator does not have any validation errors when IsFavorite is false and IsDailyOffer is false.
-    /// </summary>
     [Fact]
+    [Test]
     public void Should_Not_Have_Error_When_IsFavorite_Is_False_And_IsDailyOffer_Is_False()
     {
         // Arrange
@@ -27,62 +20,49 @@ public class FlagsObjectValueTests
         flagsObjectValue.SetIsBestSeller(false);
         // Act
         var result = _validator.TestValidate(flagsObjectValue);
-
         // Assert
         result.ShouldNotHaveValidationErrorFor(x => x.IsFavorite);
     }
-
-    /// <summary>
-    /// Tests that the validator has a validation error when IsFavorite is true and IsDailyOffer is true.
-    /// </summary>
+    
     [Fact]
+    [Test]
     public void Should_Have_Error_When_IsFavorite_Is_True_And_IsDailyOffer_Is_True()
     {
         // Arrange
         var flagsObjectValue = new FlagsObjectValue();
         flagsObjectValue.SetIsFavorite(true);
         flagsObjectValue.SetIsDailyOffer(true);
-
         // Act
         var result = _validator.TestValidate(flagsObjectValue);
-
         // Assert
         result.ShouldHaveValidationErrorFor(x => x.IsFavorite)
             .WithErrorMessage("Cannot be favorite and daily offer at the same time.");
     }
 
-    /// <summary>
-    /// Tests that the validator does not have any validation errors when IsBestSeller is false and IsDailyOffer is false.
-    /// </summary>
     [Fact]
+    [Test]
     public void Should_Not_Have_Error_When_IsBestSeller_Is_False_And_IsDailyOffer_Is_False()
     {
         // Arrange
         var flagsObjectValue = new FlagsObjectValue();
         flagsObjectValue.SetIsBestSeller(false);
         flagsObjectValue.SetIsDailyOffer(false);
-
         // Act
         var result = _validator.TestValidate(flagsObjectValue);
-
         // Assert
         result.ShouldNotHaveValidationErrorFor(x => x.IsBestSeller);
     }
 
-    /// <summary>
-    /// Tests that the validator has a validation error when IsBestSeller is true and IsDailyOffer is true.
-    /// </summary>
     [Fact]
+    [Test]
     public void Should_Have_Error_When_IsBestSeller_Is_True_And_IsDailyOffer_Is_True()
     {
         // Arrange
         var flagsObjectValue = new FlagsObjectValue();
         flagsObjectValue.SetIsBestSeller(true);
         flagsObjectValue.SetIsDailyOffer(true);
-
         // Act
         var result = _validator.TestValidate(flagsObjectValue);
-
         // Assert
         result.ShouldHaveValidationErrorFor(x => x.IsBestSeller)
             .WithErrorMessage("Cannot be best seller and daily offer at the same time.");
