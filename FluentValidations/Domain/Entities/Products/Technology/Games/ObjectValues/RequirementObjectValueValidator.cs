f46b121e03b56c8d8d8c839b758dcc750a571dd4
Rafael Silva <ainbot6@gmail.com>
2024-03-28 21:54:56 -0300

    feat: fluent validation created for the GeneralObjectValue class

---
+++ FluentValidations/Domain/Entities/Products/Technology/Games/ObjectValues/RequirementObjectValueValidator.cs
@@ -0,0 +1,27 @@
+﻿using Domain.Entities.Products.Technology.Games.ObjectValues;
+using FluentValidation;
+
+namespace FluentValidations.Domain.Entities.Products.Technology.Games.ObjectValues;
+
+public class RequirementObjectValueValidator: AbstractValidator<RequirementObjectValue>
+{
+    public RequirementObjectValueValidator()
+    {
+        RuleFor(x => x.MinimumRamRequirementInMb)
+            .NotEmpty().WithMessage("Minimum RAM requirement cannot be empty.")
+            .GreaterThanOrEqualTo(4).WithMessage("Minimum RAM requirement must be greater than or equal to 4 GB (4096 MB).")
+            .LessThanOrEqualTo(16).WithMessage("Minimum RAM requirement must be less than or equal to 16 GB (16384 MB).");
+        
+        RuleFor(x => x.MinimumOperatingSystemsRequired)
+            .NotEmpty().WithMessage("Minimum operating systems required cannot be empty.")
+            .MaximumLength(10).WithMessage("Minimum operating systems required must have a maximum length of 10 characters.");
+
+        RuleFor(x => x.MinimumGraphicsProcessorsRequired)
+            .NotEmpty().WithMessage("Minimum graphics processors required cannot be empty.")
+            .MaximumLength(10).WithMessage("Minimum graphics processors required must have a maximum length of 10 characters.");
+
+        RuleFor(x => x.MinimumProcessorsRequired)
+            .NotEmpty().WithMessage("Minimum processors required cannot be empty.")
+            .MaximumLength(10).WithMessage("Minimum processors required must have a maximum length of 10 characters.");
+    }
+}
