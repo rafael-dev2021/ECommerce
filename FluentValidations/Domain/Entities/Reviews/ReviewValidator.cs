afa2ac8a85d36d66c0a9e96400121ec7f4fc4462
Rafael Silva <ainbot6@gmail.com>
2024-03-29 20:40:20 -0300

    feat: fluent validation for the Review.cs class

---
+++ FluentValidations/Domain/Entities/Reviews/ReviewValidator.cs
@@ -0,0 +1,20 @@
+﻿using Domain.Entities.Reviews;
+using FluentValidation;
+
+namespace FluentValidations.Domain.Entities.Reviews;
+
+public class ReviewValidator: AbstractValidator<Review>
+{
+    public ReviewValidator()
+    {
+        RuleFor(x => x.Comment)
+            .NotEmpty().WithMessage("Comment cannot be empty.")
+            .MaximumLength(2000).WithMessage("Comment cannot be more than 2000 characters.");
+
+        RuleFor(x => x.Image)
+            .MaximumLength(250).WithMessage("Image cannot be more than 250 characters.");
+
+        RuleFor(x => x.Rating)
+            .InclusiveBetween(1, 5).WithMessage("Rating must be between 1 and 5.");
+    }
+}
