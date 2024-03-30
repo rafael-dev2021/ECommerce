46e5852609ca8e44db84f314f77a2eb1bc3f9f1e
Rafael Silva <ainbot6@gmail.com>
2024-03-29 21:07:20 -0300

    feat: fluent validation for the Order.cs class

---
+++ FluentValidations/Domain/Entities/Orders/OrderValidator.cs
@@ -0,0 +1,25 @@
+﻿using Domain.Entities.Orders;
+using FluentValidation;
+
+namespace FluentValidations.Domain.Entities.Orders;
+
+public class OrderValidator : AbstractValidator<Order>
+{
+    public OrderValidator()
+    {
+        RuleFor(x => x.TotalOrder)
+            .GreaterThan(0).WithMessage("Total order must be greater than zero.");
+
+        RuleFor(x => x.TotalItemsOrder)
+            .GreaterThan(0).WithMessage("Total items order must be greater than zero.");
+
+        RuleFor(x => x.ConfirmedOrder)
+            .NotEmpty().WithMessage("Confirmed order date cannot be empty.");
+
+        RuleFor(x => x.DispatchedOrder)
+            .NotEmpty().WithMessage("Dispatched order date cannot be empty.");
+
+        RuleFor(x => x.RequestReceived)
+            .NotEmpty().WithMessage("Request received date cannot be empty.");
+    }
+}
