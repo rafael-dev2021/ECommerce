6d3c6a15fe52573f3cc811edd5eab2bf4ab36b0f
Rafael Silva <ainbot6@gmail.com>
2024-03-29 22:51:16 -0300

    feat: created RegistrationResult.cs

---
+++ Domain/Identity/RegistrationResult.cs
@@ -0,0 +1,10 @@
+﻿namespace Domain.Identity;
+
+public class RegistrationResult
+{
+    public bool IsRegistered { get; private set; }
+    public string ErrorMessage { get; private set; } = string.Empty;
+
+    public void SetIsRegistered(bool isRegistered) => IsRegistered = isRegistered;
+    public void SetErrorMessage(string  errorMessage) => ErrorMessage = errorMessage;
+}
