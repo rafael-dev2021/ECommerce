197895352859ca03e402a15d801bbe77bfb5fbca
Rafael Silva <ainbot6@gmail.com>
2024-03-29 22:01:57 -0300

    feat: value objects created for the payment class

---
+++ Domain/Entities/Payments/ObjectValues/PaymentMethodObjectValue.cs
@@ -0,0 +1,62 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Domain.Entities.Payments.Algorithms;
+using Domain.Entities.Payments.Enums;
+using Domain.Interfaces.Payments;
+
+namespace Domain.Entities.Payments.ObjectValues;
+
+public class PaymentMethodObjectValue : IPaymentMethod
+{
+    [EnumDataType(typeof(EPaymentMethod))]
+    public string PaymentMethod { get; protected set; } = string.Empty;
+
+    [NotMapped]
+    public EPaymentMethod EPaymentMethod { get; protected set; }
+    public PaymentStatusObjectValue PaymentStatusObjectValue { get; protected set; } = new ();
+    public Guid Reference { get; protected set; }
+    public DateTime PaymentDate { get; protected set; }
+
+    private void PaymentDateConfirm() => PaymentDate = DateTime.Now;
+    private void SetReference() => Reference = Guid.NewGuid();
+
+    private void ProcessPayment(string cardNumber, EPaymentMethod method)
+    {
+        EPaymentMethod = method;
+        PaymentMethod = method.ToString();
+        bool isPaymentCompleted = CardValidateNumber
+            .ValidateCardNumber(cardNumber);
+        if (isPaymentCompleted)
+        {
+            PaymentStatusObjectValue.PaymentProcessing();
+            SetReference();
+
+            Thread.Sleep(2500);
+
+            PaymentStatusObjectValue.PaymentApproved();
+            PaymentDateConfirm();
+        }
+        else
+        {
+            PaymentStatusObjectValue?.PaymentDeclined();
+        }
+    }
+
+    public void CreditCardPaymentMethod(string cardNumber)
+    {
+        ProcessPayment(cardNumber, EPaymentMethod.CreditCard);
+    }
+
+    public void DebitCardPaymentMethod(string cardNumber)
+    {
+        ProcessPayment(cardNumber, EPaymentMethod.DebitCard);
+    }
+
+    public void BankSlipPaymentMethod()
+    {
+        EPaymentMethod = EPaymentMethod.BankSlip;
+        PaymentMethod = EPaymentMethod.BankSlip.ToString();
+        SetReference();
+        PaymentDateConfirm();
+    }
+}
