a282a9405b3e545df80c583ef3094a677475192f
Rafael Silva <ainbot6@gmail.com>
2024-03-30 19:57:59 -0300

    feat: create IShoppingCartItemRepository interface

---
+++ Domain/Interfaces/IShoppingCartItemRepository.cs
@@ -0,0 +1,17 @@
+﻿using Domain.Entities;
+using Domain.Entities.Cart;
+
+namespace Domain.Interfaces;
+
+public interface IShoppingCartItemRepository
+{
+    Task<IEnumerable<ShoppingCartItem>> GetShoppingCartItemsAsync();
+    Task AddItemToCartAsync(Product product, Category category);
+    Task<int> RemoveItemToCartAsync(Product product, Category category);
+    Task<int> RemoveItemAsync(Product product);
+    Task<int> GetTotalCartItemsAsync();
+    Task<decimal> GetTotalAmountCartAsync();
+    Task ClearShoppingCartAsync();
+    IEnumerable<ShoppingCartItem> ShoppingCartItems { get; set; }
+    string ShoppingCartId { get; }
+}
