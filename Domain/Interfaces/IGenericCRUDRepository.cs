5e8391ab1cfeb5abd8af8721f3253e34335f91d8
Rafael Silva <ainbot6@gmail.com>
2024-03-30 19:56:39 -0300

    feat: create IGenericCRUDRepository interface

---
+++ Domain/Interfaces/IGenericCRUDRepository.cs
@@ -0,0 +1,10 @@
+﻿namespace Domain.Interfaces;
+
+public interface IGenericCRUDRepository<T> where T : class
+{
+    Task<IEnumerable<T>> GetEntitiesAsync();
+    Task<T> GetByIdAsync(int? id);
+    Task<T> CreateAsync(T entity);
+    Task<T> UpdateAsync(T entity);
+    Task<T> DeleteAsync(T entity);
+}
