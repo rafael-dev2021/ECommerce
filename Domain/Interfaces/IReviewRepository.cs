e1da6dc53f0bbadef335d9a5f2c750615512e6a8
Rafael Silva <ainbot6@gmail.com>
2024-03-30 19:57:37 -0300

    feat: create IReviewRepository interface

---
+++ Domain/Interfaces/IReviewRepository.cs
@@ -0,0 +1,10 @@
+﻿using Domain.Entities;
+using Domain.Entities.Reviews;
+
+namespace Domain.Interfaces;
+
+public interface IReviewRepository : IGenericCRUDRepository<Review>
+{
+    Task<IEnumerable<Product>> GetProductsByReviewsAsync(string reviewStr);
+    Task<IEnumerable<Review>> GetSearchReviewAsync(string keyword);
+}
