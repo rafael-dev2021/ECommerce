f5eaafd571c4d2104b725d9373df460b6bed64b6
Rafael Silva <ainbot6@gmail.com>
2024-03-29 21:50:20 -0300

    feat: created CardValidateNumber.cs

---
+++ Domain/Entities/Payments/Algorithms/CardValidateNumber.cs
@@ -0,0 +1,30 @@
+﻿namespace Domain.Entities.Payments.Algorithms;
+
+public class CardValidateNumber
+{
+    public static bool ValidateCardNumber(string creditCardNumber)
+    {
+        creditCardNumber = new string(creditCardNumber.Where(char.IsDigit).ToArray());
+
+        int sum = 0;
+        bool alternate = false;
+
+        for (int i = creditCardNumber.Length - 1; i >= 0; i--)
+        {
+            int digit = int.Parse(creditCardNumber[i].ToString());
+
+            if (alternate)
+            {
+                digit *= 2;
+
+                if (digit > 9)
+                {
+                    digit -= 9;
+                }
+            }
+            sum += digit;
+            alternate = !alternate;
+        }
+        return (sum % 10 == 0);
+    }
+}
