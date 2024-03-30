a08294ef7bacc1d5f3a3afb7819ce96aaeb18c38
Rafael Silva <ainbot6@gmail.com>
2024-03-30 19:08:13 -0300

    feat: craeted ApplicationUser.cs

---
+++ Infra_Data/Identity/ApplicationUser.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace Infra_Data.Identity;
+
+public class ApplicationUser : IdentityUser
+{
+    public string FirstName { get; set; } = string.Empty;
+    public string LastName { get; set; }= string.Empty;
+    public string Ssn { get; set; } = string.Empty;
+    public DateTime BirthDate { get; set; }
+    public bool IsSubscribedToNewsletter { get; set; }
+}
