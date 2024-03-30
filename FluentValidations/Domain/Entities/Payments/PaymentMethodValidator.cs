86a879f4c0929e936ed06115cab815adbfb796f6
Rafael Silva <ainbot6@gmail.com>
2024-03-29 22:00:02 -0300

    feat: fluent validation for the PaymentMethod.cs class

---
+++ FluentValidations/Domain/Entities/Payments/PaymentMethodValidator.cs
@@ -0,0 +1,18 @@
+﻿using Domain.Entities.Payments;
+using FluentValidation;
+
+namespace FluentValidations.Domain.Entities.Payments;
+
+public class PaymentMethodValidator<T> : AbstractValidator<T> where T : PaymentMethod
+{
+    public PaymentMethodValidator()
+    {
+        RuleFor(x => x.Ssn)
+            .NotEmpty().WithMessage("SSN cannot be empty.")
+            .MaximumLength(11).WithMessage("SSN must have a maximum length of 11 characters.")
+            .Matches(@"^\d{9}$").WithMessage("SSN must be a 9-digit number.");
+
+        RuleFor(x => x.Amount)
+            .GreaterThan(0).WithMessage("Amount must be greater than zero.");
+    }
+}
