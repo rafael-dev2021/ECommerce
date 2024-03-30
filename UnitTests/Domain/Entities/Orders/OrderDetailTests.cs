8524c760751fc62d944d9d83aa78fff7b29b1011
Rafael Silva <ainbot6@gmail.com>
2024-03-29 21:06:49 -0300

    feat: unit test created for the OrderDetailValidator.cs class

---
+++ UnitTests/Domain/Entities/Orders/OrderDetailTests.cs
@@ -0,0 +1,94 @@
+﻿using Domain.Entities.Orders;
+using FluentValidation.TestHelper;
+using FluentValidations.Domain.Entities.Orders;
+using Xunit;
+
+namespace UnitTests.Domain.Entities.Orders;
+
+[TestFixture]
+public class OrderDetailTests
+{
+    private readonly OrderDetailValidator _validator = new();
+
+    [Fact]
+    [Test]
+    public void Quantity_WhenZero_ShouldHaveValidationError()
+    {
+        // Arrange
+        var orderDetail = new OrderDetail();
+        orderDetail.SetQuantity(0);
+        // Act
+        var result = _validator.TestValidate(orderDetail);
+        // Assert
+        result.ShouldHaveValidationErrorFor(x => x.Quantity)
+            .WithErrorMessage("Quantity must be greater than zero.");
+    }
+
+    [Fact]
+    [Test]
+    public void Quantity_WhenNegative_ShouldHaveValidationError()
+    {
+        // Arrange
+        var orderDetail = new OrderDetail();
+        orderDetail.SetQuantity(-1);
+        // Act
+        var result = _validator.TestValidate(orderDetail);
+        // Assert
+        result.ShouldHaveValidationErrorFor(x => x.Quantity)
+            .WithErrorMessage("Quantity must be greater than zero.");
+    }
+
+    [Fact]
+    [Test]
+    public void Quantity_WhenPositive_ShouldNotHaveValidationError()
+    {
+        // Arrange
+        var orderDetail = new OrderDetail();
+        orderDetail.SetQuantity(1);
+        // Act
+        var result = _validator.TestValidate(orderDetail);
+        // Assert
+        result.ShouldNotHaveValidationErrorFor(x => x.Quantity);
+    }
+
+    [Fact]
+    [Test]
+    public void Price_WhenZero_ShouldHaveValidationError()
+    {
+        // Arrange
+        var orderDetail = new OrderDetail();
+        orderDetail.SetPrice(0);
+        // Act
+        var result = _validator.TestValidate(orderDetail);
+        // Assert
+        result.ShouldHaveValidationErrorFor(x => x.Price)
+            .WithErrorMessage("Price must be greater than zero.");
+    }
+
+    [Fact]
+    [Test]
+    public void Price_WhenNegative_ShouldHaveValidationError()
+    {
+        // Arrange
+        var orderDetail = new OrderDetail();
+        orderDetail.SetPrice(-1);
+        // Act
+        var result = _validator.TestValidate(orderDetail);
+        // Assert
+        result.ShouldHaveValidationErrorFor(x => x.Price)
+            .WithErrorMessage("Price must be greater than zero.");
+    }
+
+    [Fact]
+    [Test]
+    public void Price_WhenPositive_ShouldNotHaveValidationError()
+    {
+        // Arrange
+        var orderDetail = new OrderDetail();
+        orderDetail.SetPrice(10);
+        // Act
+        var result = _validator.TestValidate(orderDetail);
+        // Assert
+        result.ShouldNotHaveValidationErrorFor(x => x.Price);
+    }
+}
