4a7f48879e84e26b7787f4f1a86c414858b30c75
Rafael Silva <ainbot6@gmail.com>
2024-03-30 19:58:12 -0300

    feat: create IPaymentRepository interface

---
+++ Domain/Interfaces/Payments/IPaymentRepository.cs
@@ -0,0 +1,15 @@
+﻿using Domain.Entities.Payments;
+
+namespace Domain.Interfaces.Payments;
+
+public interface IPaymentRepository
+{
+    Task<IEnumerable<PaymentMethod>> ListPaymentsAsync();
+    Task<IEnumerable<CreditCard>> ListPaymentCreditCardsAsync();
+    Task<IEnumerable<DebitCard>> ListPaymentDebitCardsAsync();
+
+    Task<PaymentMethod> GetByIdPaymentAsync(int? id);
+    Task<CreditCard> GetByIdPaymentCreditCardAsync(Guid? id);
+    Task<DebitCard> GetByIdPaymentDebitCardAsync(Guid? id);
+    //Task<IEnumerable<Payment>> ListPaymentBankSlipAsync();
+}
