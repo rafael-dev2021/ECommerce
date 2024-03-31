50850d11134d679cbe0d9a93fc81ed7df33487f2
Rafael Silva <ainbot6@gmail.com>
2024-03-30 21:38:24 -0300

    feat: update AppDbContext.cs

--- Infra_Data/Context/AppDbContext.cs
+++ Infra_Data/Context/AppDbContext.cs
@@ -15,21 +15,20 @@ using Microsoft.EntityFrameworkCore;
 namespace Infra_Data.Context;
 
 public class AppDbContext(DbContextOptions<AppDbContext> options) : IdentityDbContext<ApplicationUser>(options)
-
 {
-    public DbSet<Category>? Categories { get; set; } = null;
-    public DbSet<Product>?  Products { get; set; } = null;
-    public DbSet<Order>?  Orders { get; set; } = null;
-    public DbSet<OrderDetail>?  OrdersDetails { get; set; } = null;
-    public DbSet<Review>?  Reviews { get; set; } = null;
-    public DbSet<PaymentMethod>?  PaymentMethods { get; set; } = null;
-    public DbSet<CreditCard>?  CreditCards { get; set; } = null;
-    public DbSet<DebitCard>?  DebitCards { get; set; } = null;
-    public DbSet<ShoppingCartItem>?  ShoppingCartItems { get; set; } = null;
-    public DbSet<Shirt>?  Shirts { get; set; } = null;
-    public DbSet<Shoe>?  Shoes { get; set; } = null;
-    public DbSet<Game>?  Games { get; set; } = null;
-    public DbSet<Smartphone>?  Smartphones { get; set; } = null;
+    public DbSet<Category> Categories { get; set; } = null;
+    public DbSet<Product> Products { get; set; } = null;
+    public DbSet<Order> Orders { get; set; } = null;
+    public DbSet<OrderDetail> OrdersDetails { get; set; } = null;
+    public DbSet<Review> Reviews { get; set; } = null;
+    public DbSet<PaymentMethod> PaymentMethods { get; set; } = null;
+    public DbSet<CreditCard> CreditCards { get; set; } = null;
+    public DbSet<DebitCard> DebitCards { get; set; } = null;
+    public DbSet<ShoppingCartItem> ShoppingCartItems { get; set; } = null;
+    public DbSet<Shirt> Shirts { get; set; } = null;
+    public DbSet<Shoe> Shoes { get; set; } = null;
+    public DbSet<Game> Games { get; set; } = null;
+    public DbSet<Smartphone> Smartphones { get; set; } = null;
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
