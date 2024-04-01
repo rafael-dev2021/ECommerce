fdffa1ebabd63516edf727b94b5148c3e5702f3a
Rafael Silva <ainbot6@gmail.com>
2024-04-01 01:34:32 -0300

    feat: add customization program.cs

--- WebUI/Program.cs
+++ WebUI/Program.cs
@@ -1,27 +1,58 @@
+using Infra_Ioc;
+using Infra_Ioc.Identity;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
 builder.Services.AddControllersWithViews();
+
+builder.Services.AddDomainInfrastructureModule(builder.Configuration);
+
+builder.Services.AddEnUSCultureInfoDI();
+
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
 
+
+await SeedData.SeedUsersRoles(app);
+
+app.UseRequestLocalization();
 app.UseHttpsRedirection();
 app.UseStaticFiles();
 
 app.UseRouting();
+app.UseSession();
 
+app.UseAuthentication();
 app.UseAuthorization();
+
+
+app.UseStatusCodePagesWithReExecute("/Home/Error", "?statusCode={0}");
+
+app.MapControllerRoute(
+    name: "areas",
+    pattern: "{area:exists}/{controller=Admin}/{action=Index}/{id?}"
+);
+
+app.MapControllerRoute(
+    name: "adminOrderProducts",
+    pattern: "{area:exists}/{controller=AdminOrder}/{action=Index}/{id}/{*catchall}",
+    defaults: new { action = "Index" },
+    constraints: new { id = @"(.*)" } 
+);
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
+    pattern: "{controller=Home}/{action=Index}/{id?}"
+);
 
 app.Run();
+
+
+
+
