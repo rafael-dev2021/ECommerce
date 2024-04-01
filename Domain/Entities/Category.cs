cd543e7dcd366fbf7f2ddc6a93544b7183060817
Rafael Silva <ainbot6@gmail.com>
2024-04-01 02:10:05 -0300

    modified Category.cs

--- Domain/Entities/Category.cs
+++ Domain/Entities/Category.cs
@@ -1,10 +1,10 @@
 ﻿namespace Domain.Entities;
 
-public sealed class Category(int id, string name, string imagesUrl, bool isActive)
+public sealed class Category(int id, string name, string imageUrl, bool isActive)
 {
     public int Id { get; private set; } = id;
     public string Name { get; private set; } = name;
-    public string ImageUrl { get; private set; } = imagesUrl;
+    public string ImageUrl { get; private set; } = imageUrl;
     public bool IsActive { get; private set; } = isActive;
     public ICollection<Product> Products { get; } = [];
 }
