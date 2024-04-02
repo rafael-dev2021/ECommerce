920d39fc830474448b11e594bcd9bd84634d3893
Rafael Silva <ainbot6@gmail.com>
2024-04-01 21:59:00 -0300

    feat: Adds domain validation configuration using FluentValidation and dependency injection.

---
+++ Infra_Ioc/DomainFluentValidationDI.cs
@@ -0,0 +1,34 @@
+﻿using FluentValidation.AspNetCore;
+using Infra_Ioc.FluentValidations.Domain.Cart;
+using Infra_Ioc.FluentValidations.Domain.Deliveries;
+using Infra_Ioc.FluentValidations.Domain.Identity;
+using Infra_Ioc.FluentValidations.Domain.ObjectValues;
+using Infra_Ioc.FluentValidations.Domain.Orders;
+using Infra_Ioc.FluentValidations.Domain.Payments;
+using Infra_Ioc.FluentValidations.Domain.Products.Fashion.ObjectValues;
+using Infra_Ioc.FluentValidations.Domain.Products.Technology.ObjectValues;
+using Infra_Ioc.FluentValidations.Domain.Reviews;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Infra_Ioc;
+
+public static class DomainFluentValidationDI
+{
+    public static IServiceCollection AddDomainFluentValidationDI(this IServiceCollection services)
+    {
+        services.AddShirtObjectValuesValidator();
+        services.AddShoeObjectValuesValidator();
+        services.AddGameObjectValuesValidator();
+        services.AddSmartphoneObjectValuesValidator();
+        services.AddCardValidatorDependecyInjection();
+        services.AddOrderValidatorDependecyInjection();
+        services.AddProductObjectValueValidator();
+        services.AddUserDeliveryObjectValueValidator();
+        services.AddShoppingCartItemValidatorDi();
+        services.AddReviewValidatorDi();
+        services.AddSeedUserUpdateValidatorDi();
+        
+        services.AddFluentValidationAutoValidation();
+        return services;
+    }
+}
