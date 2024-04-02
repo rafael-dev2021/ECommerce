5e2507fb4328c28ac84e5694146466ff9403bd83
Rafael Silva <ainbot6@gmail.com>
2024-04-01 21:50:10 -0300

    feat: add services for dtos

---
+++ Application/Services/Entities/Products/Fashion/ShirtDtoService.cs
@@ -0,0 +1,55 @@
+﻿using Application.CQRS.Command.Products.Fashion.T_Shirts;
+using Application.CQRS.Queries.Products.Fashion.T_Shirts;
+using Application.Dtos.Products.Fashion.T_Shirts;
+using Application.Interfaces.Products.Fashion;
+using AutoMapper;
+using MediatR;
+
+namespace Application.Services.Entities.Products.Fashion;
+
+public class ShirtDtoService(IMediator mediator, IMapper mapper) : IShirtDtoService
+{
+    private readonly IMediator _mediator = mediator;
+    private readonly IMapper _mapper = mapper;
+    
+    
+    public async Task<IEnumerable<ShirtDto>> GetEntitiesDtoAsync()
+    {
+        var getProducts = new ShirtsQueries();
+        var result = await _mediator.Send(getProducts);
+        
+        return _mapper.Map<IEnumerable<ShirtDto>>(result) ?? Enumerable.Empty<ShirtDto>();
+    }
+
+    public async Task<ShirtDto> GetByIdAsync(int? id)
+    {
+        if (id == null)
+            throw new ArgumentNullException(nameof(id));
+        
+        var getProductId = new GetByIdShirtQuery(id.Value);
+        var result = await _mediator.Send(getProductId);
+        
+        return _mapper.Map<ShirtDto>(result); 
+    }
+
+    public async Task AddAsync(ShirtDto entityDto)
+    {
+        var addProduct = _mapper.Map<CreateShirtCommand>(entityDto);
+        await _mediator.Send(addProduct);
+    }
+
+    public  async Task UpdateAsync(ShirtDto entityDto)
+    {
+        var updateProduct = _mapper.Map<UpdateShirtCommand>(entityDto);
+        await _mediator.Send(updateProduct);
+    }
+
+    public async Task DeleteAsync(int? id)
+    {
+        if (id == null)
+            throw new ArgumentNullException(nameof(id));
+        
+        var deleteProduct = new RemoveShirtCommand(id.Value);
+        await _mediator.Send(deleteProduct);
+    }
+}
---
+++ Application/Services/Entities/Products/Technology/GameDtoService.cs
@@ -0,0 +1,55 @@
+﻿using Application.CQRS.Command.Products.Technology.Games;
+using Application.CQRS.Queries.Products.Technology.Games;
+using Application.Dtos.Products.Technology.Games;
+using Application.Interfaces.Products.Technology;
+using AutoMapper;
+using MediatR;
+
+namespace Application.Services.Entities.Products.Technology;
+
+public class GameDtoService(IMapper mapper, IMediator mediator) : IGameDtoService
+{
+    private readonly IMapper _mapper = mapper;
+    private readonly IMediator _mediator = mediator;
+
+
+    public async Task<IEnumerable<GameDto>> GetEntitiesDtoAsync()
+    {
+        var getProducts = new GamesQueries();
+        var result = await _mediator.Send(getProducts);
+
+        return _mapper.Map<IEnumerable<GameDto>>(result) ?? Enumerable.Empty<GameDto>();
+    }
+
+    public async Task<GameDto> GetByIdAsync(int? id)
+    {
+        if (id == null)
+            throw new ArgumentNullException(nameof(id));
+        
+        var getProductId = new GetByIdGameQuery(id.Value);
+        var result = await _mediator.Send(getProductId);
+
+        return _mapper.Map<GameDto>(result);
+    }
+
+    public async Task AddAsync(GameDto entityDto)
+    {
+        var addProduct = _mapper.Map<CreateGameCommand>(entityDto);
+        await _mediator.Send(addProduct);
+    }
+
+    public async Task UpdateAsync(GameDto entityDto)
+    {
+        var updateProduct = _mapper.Map<UpdateGameCommand>(entityDto);
+        await _mediator.Send(updateProduct);
+    }
+
+    public async Task DeleteAsync(int? id)
+    {
+        if (id == null)
+            throw new ArgumentNullException(nameof(id));
+        
+        var deleteProduct = new RemoveGameCommand(id.Value);
+        await _mediator.Send(deleteProduct);
+    }
+}
