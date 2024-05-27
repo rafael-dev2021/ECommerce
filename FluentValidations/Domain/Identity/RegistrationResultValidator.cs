9f3e73f133d57ca7f91c13a2b058e23933ff51a9
Rafael Silva <ainbot6@gmail.com>
2024-05-27 16:38:57 -0300

    feat: add new fluent validator for RegistrationResultValidator class

---
+++ FluentValidations/Domain/Identity/RegistrationResultValidator.cs
@@ -0,0 +1,22 @@
+﻿using Domain.Identity;
+using FluentValidation;
+
+namespace FluentValidations.Domain.Identity;
+
+public class RegistrationResultValidator : AbstractValidator<RegistrationResult>
+{
+    public RegistrationResultValidator()
+    {
+        RuleFor(x => x.IsRegistered)
+            .NotEqual(false).WithMessage("Registration failed.")
+            .When(x => !x.IsRegistered);
+
+        RuleFor(x => x.ErrorMessage)
+            .NotEmpty().WithMessage("Error message cannot be empty if registration failed.")
+            .When(x => !x.IsRegistered);
+
+        RuleFor(x => x.ErrorMessage)
+            .Empty().WithMessage("Error message must be empty if registration succeeded.")
+            .When(x => x.IsRegistered);
+    }
+}
