3be4ede2ee3ce087761bf3b8b109153f289d1aba
Rafael Silva <ainbot6@gmail.com>
2024-05-27 16:39:49 -0300

    test: add new unit tests AuthenticationResultTests and RegistrationResultTests

---
+++ UnitTests/Domain/Identity/RegistrationResultTests.cs
@@ -0,0 +1,109 @@
+﻿using Domain.Identity;
+using FluentValidation.TestHelper;
+using FluentValidations.Domain.Identity;
+using Xunit;
+
+namespace UnitTests.Domain.Identity
+{
+    public class RegistrationResultTests
+    {
+        private readonly RegistrationResultValidator _validator = new();
+
+        [Fact]
+        [Test]
+        public void IsRegistered_WhenFalse_ShouldHaveValidationError()
+        {
+            // Arrange
+            var model = new RegistrationResult();
+            model.SetIsRegistered(false);
+
+            // Act
+            var result = _validator.TestValidate(model);
+
+            // Assert
+            result.ShouldHaveValidationErrorFor(x => x.IsRegistered)
+                .WithErrorMessage("Registration failed.");
+        }
+
+        [Fact]
+        [Test]
+        public void ErrorMessage_WhenEmptyAndNotRegistered_ShouldHaveValidationError()
+        {
+            // Arrange
+            var model = new RegistrationResult();
+            model.SetIsRegistered(false);
+            model.SetErrorMessage("");
+
+            // Act
+            var result = _validator.TestValidate(model);
+
+            // Assert
+            result.ShouldHaveValidationErrorFor(x => x.ErrorMessage)
+                .WithErrorMessage("Error message cannot be empty if registration failed.");
+        }
+
+        [Fact]
+        [Test]
+        public void ErrorMessage_WhenNotEmptyAndRegistered_ShouldHaveValidationError()
+        {
+            // Arrange
+            var model = new RegistrationResult();
+            model.SetIsRegistered(true);
+            model.SetErrorMessage("Some error message");
+
+            // Act
+            var result = _validator.TestValidate(model);
+
+            // Assert
+            result.ShouldHaveValidationErrorFor(x => x.ErrorMessage)
+                .WithErrorMessage("Error message must be empty if registration succeeded.");
+        }
+
+        [Fact]
+        [Test]
+        public void IsRegistered_WhenTrue_ShouldNotHaveValidationError()
+        {
+            // Arrange
+            var model = new RegistrationResult();
+            model.SetIsRegistered(true);
+
+            // Act
+            var result = _validator.TestValidate(model);
+
+            // Assert
+            result.ShouldNotHaveValidationErrorFor(x => x.IsRegistered);
+        }
+
+        [Fact]
+        [Test]
+        public void ErrorMessage_WhenEmptyAndRegistered_ShouldNotHaveValidationError()
+        {
+            // Arrange
+            var model = new RegistrationResult();
+            model.SetIsRegistered(true);
+            model.SetErrorMessage("");
+
+            // Act
+            var result = _validator.TestValidate(model);
+
+            // Assert
+            result.ShouldNotHaveValidationErrorFor(x => x.ErrorMessage);
+        }
+
+        [Fact]
+        [Test]
+        public void ErrorMessage_WhenNotEmptyAndNotRegistered_ShouldNotHaveValidationError()
+        {
+            // Arrange
+            var model = new RegistrationResult();
+            model.SetIsRegistered(false);
+            model.SetErrorMessage("Some error message");
+
+            // Act
+            var result = _validator.TestValidate(model);
+
+            // Assert
+            result.ShouldNotHaveValidationErrorFor(x => x.ErrorMessage);
+        }
+    }
+}
