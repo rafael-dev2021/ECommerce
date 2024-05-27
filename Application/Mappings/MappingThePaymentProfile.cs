5fe7e9c146fd56136bd93ca2af7eac6465cef8a7
Rafael Silva <ainbot6@gmail.com>
2024-05-27 20:11:28 -0300

    feat: add new mapping MappingThePaymentProfile

---
+++ Application/Mappings/MappingThePaymentProfile.cs
@@ -0,0 +1,17 @@
+﻿using Application.Dtos.PaymentsDto;
+using AutoMapper;
+using Domain.Entities.Payments;
+
+namespace Application.Mappings;
+
+public class MappingThePaymentProfile : Profile
+{
+    public MappingThePaymentProfile()
+    {
+        CreateMap<Card, CardDto>().ReverseMap();
+        CreateMap<CreditCard, CreditCardDto>().ReverseMap();
+        CreateMap<DebitCard, DebitCardDto>().ReverseMap();
+        CreateMap<PaymentMethod, PaymentMethodDto>().ReverseMap();
+        CreateMap<Payment, PaymentDto>().ReverseMap();
+    }
+}
