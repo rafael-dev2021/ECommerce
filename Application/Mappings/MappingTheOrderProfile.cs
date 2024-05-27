e841e2c4074b81c707843a067cf8d673ac773480
Rafael Silva <ainbot6@gmail.com>
2024-05-27 19:48:33 -0300

    feat: add new mapping MappingTheOrderProfile

---
+++ Application/Mappings/MappingTheOrderProfile.cs
@@ -0,0 +1,21 @@
+﻿using Application.Dtos.OrderDtos;
+using AutoMapper;
+using Domain.Entities.Orders;
+
+namespace Application.Mappings;
+
+public class MappingTheOrderProfile : Profile
+{
+    public MappingTheOrderProfile()
+    {
+        CreateMap<Order, OrderDTo>()
+            .ForMember(dest => dest.OrderDetails, opt => opt.MapFrom(src => src.OrderDetails))
+            .ReverseMap();
+
+        CreateMap<OrderDetail, OrderDetailDto>()
+            .ForMember(dest => dest.Product, opt => opt.MapFrom(src => src.Product))
+            .ForMember(dest => dest.Order, opt => opt.MapFrom(src => src.Order))
+            .ReverseMap();
+
+    }
+}
