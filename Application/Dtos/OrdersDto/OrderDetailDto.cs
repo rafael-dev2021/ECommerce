717828cebefe567fb6e2d8b33cec5a2eeb10036a
Rafael Silva <ainbot6@gmail.com>
2024-05-27 19:47:45 -0300

    feat: add new dtos

---
+++ Application/Dtos/OrdersDto/OrderDetailDto.cs
@@ -0,0 +1,14 @@
+﻿using Application.Dtos.PaymentsDto;
+
+namespace Application.Dtos.OrderDtos;
+
+public record OrderDetailDto(
+    int Id,
+    int Quantity,
+    decimal Price,
+    int ProductId,
+    ProductDto Product,
+    int OrderId,
+    OrderDTo Order,
+    int PaymentMethodId,
+    PaymentMethodDto PaymentMethod){}
---
+++ Application/Dtos/OrdersDto/SalesByMonthDto.cs
@@ -0,0 +1,9 @@
+﻿namespace Application.Dtos.OrderDtos;
+
+public record SalesByMonthDto
+{
+    public int Year { get; set; }
+    public int Month { get; set; }
+    public decimal TotalSales { get; set; }
+}
+
