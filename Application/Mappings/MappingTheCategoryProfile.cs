4f76dd29729526b8a88cf959fcbfd5470dcbb457
Rafael Silva <ainbot6@gmail.com>
2024-05-27 18:07:12 -0300

    feat: add new mapping MappingTheCategoryProfile

---
+++ Application/Mappings/MappingTheCategoryProfile.cs
@@ -0,0 +1,13 @@
+﻿using Application.Dtos;
+using AutoMapper;
+using Domain.Entities;
+
+namespace Application.Mappings;
+
+public class MappingTheCategoryProfile : Profile
+{
+    public MappingTheCategoryProfile()
+    {
+        CreateMap<Category, CategoryDto>().ReverseMap();
+    }
+}
