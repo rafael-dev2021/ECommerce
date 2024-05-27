6fde2b84de525a875cae32507396fe6d1766ed62
Rafael Silva <ainbot6@gmail.com>
2024-05-27 20:11:49 -0300

    feat: add new interface IPaymentDtoService

---
+++ Application/Interfaces/IPaymentDtoService.cs
@@ -0,0 +1,14 @@
+﻿using Application.Dtos.PaymentsDto;
+
+namespace Application.Interfaces;
+
+public interface IPaymentDtoService
+{
+    Task<IEnumerable<PaymentMethodDto>> ListPaymentsDtoAsync();
+    Task<IEnumerable<CreditCardDto>> ListPaymentCreditCardsDtoAsync();
+    Task<IEnumerable<DebitCardDto>> ListPaymentDebitCardsDtoAsync();
+
+    Task<PaymentMethodDto> GetByIdPaymentDtoAsync(int? id);
+    Task<CreditCardDto> GetByIdPaymentCreditCardDtoAsync(Guid? id);
+    Task<DebitCardDto> GetByIdPaymentDebitCardDtoAsync(Guid? id);
+}
