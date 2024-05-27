1045b9fb943803a5a13e00f346f6d3b46db9f7ff
Rafael Silva <ainbot6@gmail.com>
2024-05-27 18:07:22 -0300

    feat: add new mapping MappingTheCategoryProfile

--- Infra_Ioc/Application/ApplicationMappingProfile.cs
+++ Infra_Ioc/Application/ApplicationMappingProfile.cs
@@ -7,6 +7,7 @@ public static class ApplicationMappingProfile
 {
     public static IServiceCollection AddApplicationMappingProfile(this IServiceCollection services)
     {
+        services.AddAutoMapper(typeof(MappingTheCategoryProfile));
         services.AddAutoMapper(typeof(MappingTheProductProfile));
         return services;
     }
