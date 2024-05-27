186e90209bb96c0f92282ac43324bcba7215d5a6
Rafael Silva <ainbot6@gmail.com>
2024-05-27 19:48:04 -0300

    feat: add new interface IOrderDtoService

---
+++ Application/Interfaces/IOrderDtoService.cs
@@ -0,0 +1,20 @@
+﻿using Application.Dtos.OrderDtos;
+using Domain.Entities.Payments.Enums;
+
+namespace Application.Interfaces;
+
+public interface IOrderDtoService
+{
+    Task<IEnumerable<OrderDto>> GetOrdersDtoAsync();
+    IQueryable<OrderDto> GetPagingListOrdersDto(string filter);
+    Task<IEnumerable<OrderDto>> FindByOrderConfirmDateDtoAsync(DateTime? minDate, DateTime? maxDate);
+    Task<IEnumerable<OrderDto>> FindByOrderDispatchedDateDtoAsync(DateTime? minDate, DateTime? maxDate);
+    Task<IEnumerable<OrderDto>> FindByOrderRequestReceivedDateDtoAsync(DateTime? minDate, DateTime? maxDate);
+    List<SalesByMonthDto> GetSalesByMonth(IEnumerable<OrderDto> orderDtos);
+    Task<IEnumerable<OrderDetailDto>> GetOrdersDetailsAsync();
+    Task<OrderDto> GetByIdAsync(int? id);
+    Task AddOrder(OrderDto orderDto, EPaymentMethod ePaymentMethod);
+    Task UpdateOrderPropertyAsync(OrderDto orderDto);
+    Task DeleteOrder(int? id);
+    Task<decimal> Average();
+}
