a9bf444697c0d1fd766cbff416a29c95d668f9de
Rafael Silva <ainbot6@gmail.com>
2024-05-27 18:06:26 -0300

    feat: add new interfaces

---
+++ Application/Interfaces/ICategoryDtoService.cs
@@ -0,0 +1,8 @@
+﻿using Application.Dtos;
+
+namespace Application.Interfaces;
+
+public interface ICategoryDtoService : IGenericCRUDServiceDTO<CategoryDto>
+{
+    Task<List<CategoryWithProductCountDto>> GetCategoriesWithProductDtoCountAsync();
+}
---
+++ Application/Interfaces/IGenericCRUDServiceDTO.cs
@@ -0,0 +1,10 @@
+﻿namespace Application.Interfaces;
+
+public interface IGenericCRUDServiceDTO<T> where T : class
+{
+    Task<IEnumerable<T>> GetEntitiesAsync();
+    Task<T> GetByIdAsync(int? id);
+    Task AddAsync(T entity);
+    Task UpdateAsync(T entity);
+    Task DeleteAsync(int? id);
+}
