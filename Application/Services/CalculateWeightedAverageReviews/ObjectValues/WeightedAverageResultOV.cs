8fda60282edc28ae0f0f94e038526660da5054a9
Rafael Silva <ainbot6@gmail.com>
2024-05-27 21:03:16 -0300

    feat: add business rule for valuation averages

---
+++ Application/Services/CalculateWeightedAverageReviews/ObjectValues/WeightedAverageResultOV.cs
@@ -0,0 +1,7 @@
+﻿namespace Application.Services.CalculateWeightedAverageReviews.ObjectValues;
+
+public class WeightedAverageResultOV
+{
+    public double WeightedAverage { get; set; }
+    public int CountReviews { get; set; }
+}
