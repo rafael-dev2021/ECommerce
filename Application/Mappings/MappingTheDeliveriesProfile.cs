5e15c6888e30abbb9ef19db6815c20a7ff4dace1
Rafael Silva <ainbot6@gmail.com>
2024-05-27 21:02:08 -0300

    feat: add new mapping MappingTheDeliveriesProfile

---
+++ Application/Mappings/MappingTheDeliveriesProfile.cs
@@ -0,0 +1,14 @@
+﻿using Application.Dtos.DeliveriesDto;
+using AutoMapper;
+using Domain.Entities.Deliveries;
+
+namespace Application.Mappings;
+
+public class MappingTheDeliveriesProfile : Profile
+{
+    public MappingTheDeliveriesProfile()
+    {
+        CreateMap<DeliveryAddress, DeliveryAddressDto>().ReverseMap();
+        CreateMap<UserDelivery, UserDeliveryDto>().ReverseMap();
+    }
+}
