f161759cb37b40fc2f036cf6081c0a947540b671
Rafael Silva <ainbot6@gmail.com>
2024-05-27 21:05:02 -0300

    feat: add business rule to list products at higher prices than others

---
+++ Application/Services/PriceIsHigherThan/PriceIsHigherThanServiceBooleans.cs
@@ -0,0 +1,25 @@
+﻿using Application.Dtos;
+
+namespace Application.Services.PriceIsHigherThan;
+
+public class PriceIsHigherThanServiceBooleans
+{
+    public ProductDto? ProductDto { get; set; }
+    public bool IsPriceHigherThanTwoThousand
+    {
+        get { return ProductDto?.PriceObjectValue?.Price >= 2000; }
+    }
+    public bool IsPriceIsBetweenTwoHundredAndAThousand
+    {
+        get
+        {
+            return
+                ProductDto?.PriceObjectValue?.Price >= 200 &&
+                ProductDto?.PriceObjectValue?.Price <= 1000;
+        }
+    }
+    public bool IsPriceIsLowerThanOneHundred
+    {
+        get { return ProductDto?.PriceObjectValue?.Price <= 100; }
+    }
+}
