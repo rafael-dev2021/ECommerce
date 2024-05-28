9a225999bc1d2bf0c30f02df0aa036441535a886
Rafael Silva <ainbot6@gmail.com>
2024-05-27 21:03:40 -0300

    feat: add business rule to count products by price

---
+++ Application/Services/CountProductByPrice/CountProductByPriceValuable.cs
@@ -0,0 +1,8 @@
+﻿namespace Application.Services.CountProductByPrice;
+
+public class CountProductByPriceValuable
+{
+    public int CountPriceIsHigherThanTwoThousand { get; set; }
+    public int CountPriceIsBetweenTwoHundredAndAThousand { get; set; }
+    public int CountPriceIsLowerThanOneHundred { get; set; }
+}
---
+++ Application/Services/CountProductByPrice/Interfaces/ICountProductsByPriceService.cs
@@ -0,0 +1,8 @@
+﻿namespace Application.Services.CountProductByPrice.Interfaces;
+
+public interface ICountProductsByPriceService
+{
+    Task<int> CountingProductsAbovePriceAsync(decimal price);
+    Task<int> CountingProductsBelowPriceAsync(decimal price);
+    Task<int> CountingProductsAboveOrBelowPriceAsync(decimal price, decimal secondPrice);
+}
