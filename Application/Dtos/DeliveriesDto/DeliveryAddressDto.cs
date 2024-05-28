b238b451d5d9f511cabf75058125721bf267b755
Rafael Silva <ainbot6@gmail.com>
2024-05-27 21:01:51 -0300

    feat: add new dtos

---
+++ Application/Dtos/DeliveriesDto/DeliveryAddressDto.cs
@@ -0,0 +1,11 @@
+﻿namespace Application.Dtos.DeliveriesDto;
+
+public record DeliveryAddressDto(
+    string Country,
+    string Address,
+    string Complement,
+    string ZipCode,
+    string State,
+    string City,
+    string Neighborhood) { }
+
