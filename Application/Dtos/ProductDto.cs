9b26f9d51feb237fca7fbd6d87eee01a9e2941fa
Rafael Silva <ainbot6@gmail.com>
2024-05-28 17:36:55 -0300

    refactor: small change

--- Application/Dtos/ProductDto.cs
+++ Application/Dtos/ProductDto.cs
@@ -12,12 +12,12 @@ public record ProductDto
 {
     public int Id { get; set; }
     [DisplayName("Name")] public string? Name { get; set; }
-    [DisplayName("Description")] public string? Description { get; set; } 
-    [DisplayName("Images")] public List<string>? ImagesUrl { get; set; } 
+    [DisplayName("Description")] public string? Description { get; set; }
+    [DisplayName("Images")] public List<string>? ImagesUrl { get; set; }
     [DisplayName("Stock")] public int Stock { get; set; }
     [DisplayName("Categories")] public int CategoryId { get; set; }
     public Category? Category { get; set; }
-    public required ICollection<ReviewDto> Reviews { get; set; }
+    public ICollection<ReviewDto> Reviews { get; set; } = [];
 
     public DataDtoObjectValue? DataObjectValue { get; set; }
     public FlagsDtoObjectValue? FlagsObjectValue { get; set; }
