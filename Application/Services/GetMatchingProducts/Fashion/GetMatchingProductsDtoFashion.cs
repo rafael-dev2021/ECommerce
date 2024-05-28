30befc5a278bc15f64dc3e0aef4067a0a06cab0a
Rafael Silva <ainbot6@gmail.com>
2024-05-27 21:04:31 -0300

    feat: add business rule to list products with their respective models and categories

---
+++ Application/Services/GetMatchingProducts/Fashion/GetMatchingProductsDtoFashion.cs
@@ -0,0 +1,28 @@
+﻿using Application.Dtos;
+using Application.Dtos.Products.Fashion.Shoes;
+using Application.Dtos.Products.Fashion.T_Shirts;
+
+namespace Application.Services.GetMatchingProducts.Fashion;
+
+public class GetMatchingProductsDtoFashion
+{
+    public ProductDto? ProductDto { get; set; }
+    public required IEnumerable<ShirtDto> TshirtsDto { get; set; }
+    public required IEnumerable<ShoeDto> ShoesDto { get; set; }
+
+    public IEnumerable<T> GetMatchingProducts<T>(IEnumerable<T> productsDto) where T : ProductDto
+    {
+        return productsDto
+            .Where(x =>
+            x.SpecificationObjectValue?.Model == ProductDto?.SpecificationObjectValue?.Model &&
+            x.Category?.Name == ProductDto?.Category?.Name);
+    }
+    public IEnumerable<ShirtDto> GetMatchingTshirtDto()
+    {
+        return GetMatchingProducts(TshirtsDto);
+    }
+    public IEnumerable<ShoeDto> GetMatchingShoesDto()
+    {
+        return GetMatchingProducts(ShoesDto);
+    }
+}
---
+++ Application/Services/GetMatchingProducts/Technology/GetMatchingProductsDtoTechnology.cs
@@ -0,0 +1,29 @@
+﻿using Application.Dtos;
+using Application.Dtos.Products.Technology.Games;
+using Application.Dtos.Products.Technology.Smartphones;
+
+namespace Application.Services.GetMatchingProducts.Technology;
+
+public class GetMatchingProductsDtoTechnology
+{
+    public ProductDto? ProductDto { get; set; }
+    public required IEnumerable<SmartphoneDto> SmartphonesDto { get; set; }
+    public required IEnumerable<GameDto> GamesDto { get; set; }
+
+    public IEnumerable<T> GetMatchingProducts<T>(IEnumerable<T> productsDto) where T : ProductDto
+    {
+        return productsDto
+            .Where(x =>
+            x.SpecificationObjectValue?.Model == ProductDto?.SpecificationObjectValue?.Model &&
+            x.Category?.Name == ProductDto?.Category?.Name);
+    }
+    public IEnumerable<SmartphoneDto> GetMatchingSmartphonesDto()
+    {
+        return GetMatchingProducts(SmartphonesDto);
+    }
+
+    public IEnumerable<GameDto> GetMatchingGamesDto()
+    {
+        return GetMatchingProducts(GamesDto);
+    }
+}
