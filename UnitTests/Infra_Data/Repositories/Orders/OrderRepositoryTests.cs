85648dda33e8dc8c83bc49c7cdc7a98b5b8f2e9c
Rafael Silva <ainbot6@gmail.com>
2024-05-29 23:18:34 -0300

    test: add new unit tests

---
+++ UnitTests/Infra_Data/Repositories/Orders/OrderRepositoryTests.cs
@@ -0,0 +1,352 @@
+﻿using Domain.Entities.Deliveries;
+using Domain.Entities.Orders;
+using Domain.Entities.Payments;
+using Domain.Entities.Payments.Enums;
+using Domain.Entities.Payments.ObjectValues;
+using Domain.Interfaces;
+using Infra_Data.Context;
+using Infra_Data.Repositories.Orders;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Diagnostics;
+using NSubstitute;
+using Xunit;
+using Assert = Xunit.Assert;
+
+namespace UnitTests.Infra_Data.Repositories.Orders;
+
+public class OrderRepositoryTests
+{
+    private static AppDbContext GetInMemoryDbContext()
+    {
+        var options = new DbContextOptionsBuilder<AppDbContext>()
+            .UseInMemoryDatabase(databaseName: "TestDatabase")
+            .ConfigureWarnings(w => w.Ignore(InMemoryEventId.TransactionIgnoredWarning))
+            .Options;
+
+        var context = new AppDbContext(options);
+        return context;
+    }
+
+
+    private static IShoppingCartItemRepository GetMockShoppingCartItemRepository()
+    {
+        return Substitute.For<IShoppingCartItemRepository>();
+    }
+
+    [Fact]
+    public async Task GetEntitiesAsync_ShouldReturnOrders()
+    {
+        // Arrange
+        var context = GetInMemoryDbContext();
+        var shoppingCartItemRepository = GetMockShoppingCartItemRepository();
+        var repository = new OrderRepository(context, shoppingCartItemRepository);
+
+        var order1 = new Order();
+        order1.SetId(1);
+        order1.SetOrderDetails([new()]);
+        order1.SetDeliveryAddress(new DeliveryAddress());
+        order1.SetUserDelivery(new UserDelivery());
+
+        var paymentMethod1 = new PaymentMethod();
+
+        var paymentMethodObjectValue1 = new PaymentMethodObjectValue();
+
+        paymentMethod1.SetPaymentMethodObjectValue(new PaymentMethodObjectValue());
+
+        paymentMethodObjectValue1.SetPaymentStatusObjectValue(new PaymentStatusObjectValue());
+
+        paymentMethod1.SetCreditCard(new CreditCard());
+
+        paymentMethod1.SetDebitCard(new DebitCard());
+
+        order1.SetPaymentMethod(paymentMethod1);
+
+        var order2 = new Order();
+        order2.SetId(2);
+        order2.SetOrderDetails([new()]);
+        order1.SetDeliveryAddress(new DeliveryAddress());
+        order1.SetUserDelivery(new UserDelivery());
+
+        var paymentMethod2 = new PaymentMethod();
+
+        var paymentMethodObjectValue2 = new PaymentMethodObjectValue();
+
+        paymentMethod2.SetPaymentMethodObjectValue(new PaymentMethodObjectValue());
+
+        paymentMethodObjectValue2.SetPaymentStatusObjectValue(new PaymentStatusObjectValue());
+
+        paymentMethod2.SetCreditCard(new CreditCard());
+
+        paymentMethod2.SetDebitCard(new DebitCard());
+
+        order2.SetPaymentMethod(paymentMethod2);
+
+        var orders = new List<Order> { order1, order2 };
+
+        context.Orders.AddRange(orders);
+        await context.SaveChangesAsync();
+
+        // Act
+        var result = await repository.GetEntitiesAsync();
+
+        // Assert
+        Assert.Equal(2, result.Count());
+        Assert.Contains(result, o => o.Id == 1);
+        Assert.Contains(result, o => o.Id == 2);
+    }
+
+    [Fact]
+    public void GetPagingListOrders_ShouldReturnFilteredOrders()
+    {
+        // Arrange
+        var context = GetInMemoryDbContext();
+        var shoppingCartItemRepository = GetMockShoppingCartItemRepository();
+        var repository = new OrderRepository(context, shoppingCartItemRepository);
+
+        var order1 = new Order();
+        order1.SetId(1);
+        order1.UserDelivery.SetFirstName("Alice");
+
+        var order2 = new Order();
+        order2.SetId(2);
+        order2.UserDelivery.SetFirstName("Bob");
+
+        var orders = new List<Order>
+        {
+            order1,
+            order2
+        };
+
+        context.Orders.AddRange(orders);
+        context.SaveChanges();
+
+        Assert.Equal(2, context.Orders.Count());
+        Assert.Contains(context.Orders, o => o.UserDelivery.FirstName == "Alice");
+        Assert.Contains(context.Orders, o => o.UserDelivery.FirstName == "Bob");
+
+        // Act
+        var result = repository.GetPagingListOrders("Alice").ToList();
+
+        // Assert
+        Assert.Single(result);
+        Assert.Contains(result, o => o.UserDelivery.FirstName == "Alice");
+    }
+
+    [Fact]
+    public async Task GetOrdersDetailsAsync_ShouldReturnEmptyList()
+    {
+        // Arrange
+        var context = GetInMemoryDbContext();
+        var shoppingCartItemRepository = GetMockShoppingCartItemRepository();
+        var repository = new OrderRepository(context, shoppingCartItemRepository);
+
+        // Act
+        var result = await repository.GetOrdersDetailsAsync();
+
+        // Assert
+        Assert.NotNull(result);
+        Assert.Empty(result);
+    }
+
+    [Fact]
+    public async Task FindByOrderConfirmDateAsync_ShouldReturnOrders()
+    {
+        // Arrange
+        var context = GetInMemoryDbContext();
+        var shoppingCartItemRepository = GetMockShoppingCartItemRepository();
+        var repository = new OrderRepository(context, shoppingCartItemRepository);
+
+        var order1 = new Order();
+        order1.SetId(1);
+        order1.SetConfirmedOrder(DateTime.Now.AddDays(-1));
+
+        var order2 = new Order();
+        order2.SetId(2);
+        order2.SetConfirmedOrder(DateTime.Now);
+
+        var orders = new List<Order>
+        {
+            order1,
+            order2
+        };
+
+        context.Orders.AddRange(orders);
+        await context.SaveChangesAsync();
+
+        // Act
+        var result = await repository.FindByOrderConfirmDateAsync(DateTime.Now.AddDays(-2), DateTime.Now);
+
+        // Assert
+        Assert.Equal(2, result.Count());
+        Assert.Contains(result, o => o.Id == 1);
+        Assert.Contains(result, o => o.Id == 2);
+    }
+
+    [Fact]
+    public async Task FindByOrderDispatchedDateAsync_ShouldReturnOrders()
+    {
+        // Arrange
+        var context = GetInMemoryDbContext();
+        var shoppingCartItemRepository = GetMockShoppingCartItemRepository();
+        var repository = new OrderRepository(context, shoppingCartItemRepository);
+
+        var order1 = new Order();
+        order1.SetId(1);
+        order1.SetDispatchedOrder(DateTime.Now.AddDays(-1));
+
+        var order2 = new Order();
+        order2.SetId(2);
+        order2.SetDispatchedOrder(DateTime.Now);
+
+        var orders = new List<Order>
+        {
+            order1,
+            order2
+        };
+
+        context.Orders.AddRange(orders);
+        await context.SaveChangesAsync();
+
+        // Act
+        var result = await repository.FindByOrderDispatchedDateAsync(DateTime.Now.AddDays(-2), DateTime.Now);
+
+        // Assert
+        Assert.Equal(2, result.Count());
+        Assert.Contains(result, o => o.Id == 1);
+        Assert.Contains(result, o => o.Id == 2);
+    }
+
+    [Fact]
+    public async Task FindByOrderRequestReceivedDateAsync_ShouldReturnOrders()
+    {
+        // Arrange
+        var context = GetInMemoryDbContext();
+        var shoppingCartItemRepository = GetMockShoppingCartItemRepository();
+        var repository = new OrderRepository(context, shoppingCartItemRepository);
+
+        var order1 = new Order();
+        order1.SetId(1);
+        order1.SetRequestReceived(DateTime.Now.AddDays(-1));
+
+        var order2 = new Order();
+        order2.SetId(2);
+        order2.SetRequestReceived(DateTime.Now);
+
+        var orders = new List<Order>
+        {
+            order1,
+            order2
+        };
+
+        context.Orders.AddRange(orders);
+        await context.SaveChangesAsync();
+
+        // Act
+        var result = await repository.FindByOrderRequestReceivedDateAsync(DateTime.Now.AddDays(-2), DateTime.Now);
+
+        // Assert
+        Assert.Equal(2, result.Count());
+        Assert.Contains(result, o => o.Id == 1);
+        Assert.Contains(result, o => o.Id == 2);
+    }
+
+    [Fact]
+    public async Task GetByIdAsync_ShouldReturnOrder()
+    {
+        // Arrange
+        var context = GetInMemoryDbContext();
+        var shoppingCartItemRepository = GetMockShoppingCartItemRepository();
+        var repository = new OrderRepository(context, shoppingCartItemRepository);
+
+        var order = new Order();
+        order.SetId(1);
+
+        context.Orders.Add(order);
+        await context.SaveChangesAsync();
+
+        // Act
+        var result = await repository.GetByIdAsync(1);
+
+        // Assert
+        Assert.NotNull(result);
+        Assert.Equal(1, result.Id);
+    }
+
+    [Fact]
+    public async Task CreateOrder_ShouldAddOrder()
+    {
+        // Arrange
+        var context = GetInMemoryDbContext();
+        var shoppingCartItemRepository = GetMockShoppingCartItemRepository();
+        var repository = new OrderRepository(context, shoppingCartItemRepository);
+
+        var order = new Order();
+        order.SetId(3);
+
+        var creditCard = new CreditCard();
+        creditCard.SetCardNumber("4111111111111111");
+        creditCard.SetCardHolderName("Test Holder");
+        creditCard.SetCardExpirationDate("12/25");
+        creditCard.SetCardCvv("123");
+        creditCard.SetSsn("123-45-6789");
+
+        var paymentMethod = new PaymentMethod();
+        paymentMethod.SetCreditCard(creditCard);
+
+        order.SetPaymentMethod(paymentMethod);
+
+        // Act
+        await repository.CreateOrder(order, EPaymentMethod.CreditCard);
+
+        // Assert
+        var orderInDb = await context.Orders.FindAsync(3);
+        Assert.NotNull(orderInDb);
+    }
+
+    [Fact]
+    public async Task UpdateAsync_ShouldUpdateOrder()
+    {
+        // Arrange
+        var context = GetInMemoryDbContext();
+        var shoppingCartItemRepository = GetMockShoppingCartItemRepository();
+        var repository = new OrderRepository(context, shoppingCartItemRepository);
+
+        var order = new Order();
+        order.SetId(1);
+
+        context.Orders.Add(order);
+        await context.SaveChangesAsync();
+
+        order.SetDispatchedOrder(DateTime.Now);
+
+        // Act
+        var result = await repository.UpdateAsync(order);
+
+        // Assert
+        Assert.Equal(DateTime.Now, result.DispatchedOrder, TimeSpan.FromMinutes(1)); // Using TimeSpan to account for small time differences
+    }
+
+    [Fact]
+    public async Task DeleteAsync_ShouldRemoveOrder()
+    {
+        // Arrange
+        var context = GetInMemoryDbContext();
+        var shoppingCartItemRepository = GetMockShoppingCartItemRepository();
+        var repository = new OrderRepository(context, shoppingCartItemRepository);
+
+        var order = new Order();
+        order.SetId(1);
+
+        context.Orders.Add(order);
+        await context.SaveChangesAsync();
+
+        // Act
+        var result = await repository.DeleteAsync(order);
+
+        // Assert
+        Assert.Equal(1, result.Id);
+
+        var orderInDb = await context.Orders.FindAsync(1);
+        Assert.Null(orderInDb);
+    }
+}
---
+++ UnitTests/Infra_Data/Repositories/ShoppingCartRepositoryTests.cs
@@ -0,0 +1,245 @@
+﻿using Domain.Entities;
+using Domain.Entities.Cart;
+using Infra_Data.Context;
+using Infra_Data.Repositories;
+using Microsoft.AspNetCore.Http;
+using Microsoft.EntityFrameworkCore;
+using NSubstitute;
+using System.Security.Claims;
+using Xunit;
+using Assert = Xunit.Assert;
+
+namespace UnitTests.Infra_Data.Repositories;
+
+public class ShoppingCartRepositoryTests
+{
+    private static AppDbContext GetInMemoryDbContext()
+    {
+        var options = new DbContextOptionsBuilder<AppDbContext>()
+            .UseInMemoryDatabase(databaseName: $"TestDatabase-{Guid.NewGuid()}")
+            .Options;
+
+        return new AppDbContext(options);
+    }
+
+    private static IHttpContextAccessor GetHttpContextAccessor(bool isAuthenticated)
+    {
+        var httpContextAccessor = Substitute.For<IHttpContextAccessor>();
+        var httpContext = new DefaultHttpContext();
+        var session = Substitute.For<ISession>();
+
+        session.Id.Returns(Guid.NewGuid().ToString());
+        httpContext.Session = session;
+
+        if (isAuthenticated)
+        {
+            var user = new ClaimsPrincipal(new ClaimsIdentity(
+            [
+                new(ClaimTypes.NameIdentifier, "test-user-id")
+            ], "mock"));
+
+            httpContext.User = user;
+        }
+
+        httpContextAccessor.HttpContext.Returns(httpContext);
+
+        return httpContextAccessor;
+    }
+
+    [Fact]
+    public void ShoppingCartId_ShouldReturnUserId_WhenUserIsAuthenticated()
+    {
+        var context = GetInMemoryDbContext();
+        var httpContextAccessor = GetHttpContextAccessor(true);
+        var repository = new ShoppingCartRepository(context, httpContextAccessor);
+
+        var shoppingCartId = repository.ShoppingCartId;
+
+        Assert.Equal("user-test-user-id", shoppingCartId);
+    }
+
+    [Fact]
+    public void ShoppingCartItems_Get_ShouldReturnItemsForCorrectShoppingCartId()
+    {
+        var context = GetInMemoryDbContext();
+        var httpContextAccessor = GetHttpContextAccessor(true);
+        var repository = new ShoppingCartRepository(context, httpContextAccessor);
+
+        var category = new Category(1, "Category1", "image.jpg", true);
+        var product = new Product(1, "Product1", "Description1", [], 10, 1);
+        var shoppingCartItem = new ShoppingCartItem();
+        shoppingCartItem.SetShoppingCartId("user-test-user-id");
+        shoppingCartItem.SetQuantity(2);
+        shoppingCartItem.Product = product;
+        shoppingCartItem.Category = category;
+
+
+        context.Products.Add(product);
+        context.Categories.Add(category);
+        context.ShoppingCartItems.Add(shoppingCartItem);
+        context.SaveChanges();
+
+        var items = repository.ShoppingCartItems;
+
+        Assert.Single(items);
+        Assert.Equal(product.Id, items.First().ProductId);
+        Assert.Equal(category.Id, items.First().CategoryId);
+        Assert.Equal(2, items.First().Quantity);
+    }
+
+    [Fact]
+    public async Task GetShoppingCartItemsAsync_ShouldReturnItemsForCorrectShoppingCartId()
+    {
+        var context = GetInMemoryDbContext();
+        var httpContextAccessor = GetHttpContextAccessor(true);
+        var repository = new ShoppingCartRepository(context, httpContextAccessor);
+
+        var category = new Category(1, "Category1", "image.jpg", true);
+        var product = new Product(1, "Product1", "Description1", [], 10, 1);
+        var shoppingCartItem = new ShoppingCartItem();
+        shoppingCartItem.SetShoppingCartId("user-test-user-id");
+        shoppingCartItem.SetQuantity(2);
+        shoppingCartItem.Product = product;
+        shoppingCartItem.Category = category;
+
+
+        context.Products.Add(product);
+        context.Categories.Add(category);
+        context.ShoppingCartItems.Add(shoppingCartItem);
+        await context.SaveChangesAsync();
+
+        var items = await repository.GetShoppingCartItemsAsync();
+
+        Assert.Single(items);
+        Assert.Equal(product.Id, items.First().ProductId);
+        Assert.Equal(category.Id, items.First().CategoryId);
+        Assert.Equal(2, items.First().Quantity);
+    }
+
+    [Fact]
+    public async Task AddItemToCartAsync_ShouldAddNewItem()
+    {
+        var context = GetInMemoryDbContext();
+        var httpContextAccessor = GetHttpContextAccessor(true);
+        var repository = new ShoppingCartRepository(context, httpContextAccessor);
+
+        var category = new Category(1, "Category1", "image.jpg", true);
+        var product = new Product(1, "Product1", "Description1", [], 10, 1);
+
+        context.Products.Add(product);
+        context.Categories.Add(category);
+        await context.SaveChangesAsync();
+
+        await repository.AddItemToCartAsync(product, category);
+
+        var items = await repository.GetShoppingCartItemsAsync();
+        Assert.Single(items);
+        var item = items.First();
+        Assert.Equal(product.Id, item.ProductId);
+        Assert.Equal(category.Id, item.CategoryId);
+        Assert.Equal(1, item.Quantity);
+    }
+
+    [Fact]
+    public async Task RemoveItemToCartAsync_ShouldRemoveItem()
+    {
+        var context = GetInMemoryDbContext();
+        var httpContextAccessor = GetHttpContextAccessor(true);
+        var repository = new ShoppingCartRepository(context, httpContextAccessor);
+
+        var category = new Category(1, "Category1", "image.jpg", true);
+        var product = new Product(1, "Product1", "Description1", [], 10, 1);
+
+        await repository.AddItemToCartAsync(product, category);
+        await repository.AddItemToCartAsync(product, category);
+
+        var quantity = await repository.RemoveItemToCartAsync(product, category);
+
+        Assert.Equal(1, quantity);
+
+        quantity = await repository.RemoveItemToCartAsync(product, category);
+
+        Assert.Equal(0, quantity);
+    }
+
+    [Fact]
+    public async Task GetTotalCartItemsAsync_ShouldReturnCorrectTotal()
+    {
+        var context = GetInMemoryDbContext();
+        var httpContextAccessor = GetHttpContextAccessor(true);
+        var repository = new ShoppingCartRepository(context, httpContextAccessor);
+
+        var category = new Category(1, "Category1", "image.jpg", true);
+        var product = new Product(1, "Product1", "Description1", [], 10, 1);
+
+        var shoppingCartItem1 = new ShoppingCartItem();
+        shoppingCartItem1.SetShoppingCartId("user-test-user-id");
+        shoppingCartItem1.SetQuantity(2);
+        shoppingCartItem1.Product = product;
+        shoppingCartItem1.Category = category;
+
+        var shoppingCartItem2 = new ShoppingCartItem();
+        shoppingCartItem2.SetShoppingCartId("user-test-user-id");
+        shoppingCartItem2.SetQuantity(2);
+        shoppingCartItem2.Product = product;
+        shoppingCartItem2.Category = category;
+
+
+        context.ShoppingCartItems.AddRange(shoppingCartItem1, shoppingCartItem2);
+        await context.SaveChangesAsync();
+
+        var totalItems = await repository.GetTotalCartItemsAsync();
+
+        Assert.Equal(4, totalItems);
+    }
+
+    [Fact]
+    public async Task ClearShoppingCartAsync_ShouldRemoveAllItems()
+    {
+        var context = GetInMemoryDbContext();
+        var httpContextAccessor = GetHttpContextAccessor(true);
+        var repository = new ShoppingCartRepository(context, httpContextAccessor);
+
+        var category = new Category(1, "Category1", "image.jpg", true);
+        var product = new Product(1, "Product1", "Description1", [], 10, 1);
+        var shoppingCartItem = new ShoppingCartItem();
+        shoppingCartItem.SetUserId("user-test-user-id");
+        shoppingCartItem.SetQuantity(2);
+        shoppingCartItem.Product = product;
+        shoppingCartItem.Category = category;
+
+        context.ShoppingCartItems.Add(shoppingCartItem);
+        await context.SaveChangesAsync();
+
+        await repository.ClearShoppingCartAsync();
+
+        var itemsAfterClearing = await context.ShoppingCartItems.Where(x => x.UserId == "user-test-user-id").ToListAsync();
+        Assert.Empty(itemsAfterClearing);
+
+        var items = await repository.GetShoppingCartItemsAsync();
+        Assert.Empty(items);
+    }
+
+    [Fact]
+    public async Task RemoveItemAsync_ShouldRemoveItem()
+    {
+        var context = GetInMemoryDbContext();
+        var httpContextAccessor = GetHttpContextAccessor(true);
+        var repository = new ShoppingCartRepository(context, httpContextAccessor);
+
+        var category = new Category(1, "Category1", "image.jpg", true);
+        var product = new Product(1, "Product1", "Description1", [], 10, 1);
+        var shoppingCartItem = new ShoppingCartItem();
+        shoppingCartItem.SetShoppingCartId("user-test-user-id");
+        shoppingCartItem.SetQuantity(2);
+        shoppingCartItem.Product = product;
+        shoppingCartItem.Category = category;
+
+        context.ShoppingCartItems.Add(shoppingCartItem);
+        await context.SaveChangesAsync();
+
+        var removedCount = await repository.RemoveItemAsync(product);
+
+        Assert.Equal(1, removedCount);
+    }
+}
