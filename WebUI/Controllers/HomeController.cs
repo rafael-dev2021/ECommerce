9bd47c3b2e8a50da3313f3ffb2af6bcd44b6f07f
Rafael Silva <ainbot6@gmail.com>
2024-06-07 19:33:58 -0300

    small change

--- WebUI/Controllers/HomeController.cs
+++ WebUI/Controllers/HomeController.cs
@@ -4,15 +4,8 @@ using WebUI.Models;
 
 namespace WebUI.Controllers;
 
-public class HomeController : Controller
+public class HomeController(ILogger<HomeController> logger) : Controller
 {
-    private readonly ILogger<HomeController> _logger;
-
-    public HomeController(ILogger<HomeController> logger)
-    {
-        _logger = logger;
-    }
-
     public IActionResult Index()
     {
         return View();
