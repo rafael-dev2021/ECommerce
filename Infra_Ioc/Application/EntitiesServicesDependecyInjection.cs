398e5351f7562f5616ac7316ce2a1decf819aba1
Rafael Silva <ainbot6@gmail.com>
2024-06-07 21:14:33 -0300

    feat: add new dependecies injections

--- Infra_Ioc/Application/EntitiesServicesDependecyInjection.cs
+++ Infra_Ioc/Application/EntitiesServicesDependecyInjection.cs
@@ -2,6 +2,14 @@
 using Application.Services.Entities;
 using Microsoft.Extensions.DependencyInjection;
 using System.Reflection;
+using Application.Services.CalculateWeightedAverageReviews;
+using Application.Services.CalculateWeightedAverageReviews.Interfaces;
+using Application.Services.CountProductByPrice;
+using Application.Services.CountProductByPrice.Interfaces;
+using Application.Services.Discounts;
+using Application.Services.Discounts.Interfaces;
+using Application.Services.PriceIsHigherThan;
+using Application.Services.PriceIsHigherThan.Interfaces;
 
 namespace Infra_Ioc.Application;
 
@@ -15,6 +23,10 @@ public static class EntitiesServicesDependecyInjection
         services.AddScoped<IShoppingCartItemDtoService, ShoppingCartItemDtoService>();
         services.AddScoped<IOrderDtoService, OrderDtoService>();
         services.AddScoped<IPaymentDtoService, PaymentDtoService>();
+        services.AddScoped<ICountProductsByPriceService, CountProductsByPriceService>();
+        services.AddScoped<IPriceIsHigherThanService, PriceIsHigherThanService>();
+        services.AddScoped<ICalculateDiscountService, CalculateDiscountService>();
+        services.AddScoped<IWeightedAverageResult, WeightedAverageCalculator>();
 
         var applicationAssembly = AppDomain.CurrentDomain.Load("Application");
         services.AddMediatR(x =>
